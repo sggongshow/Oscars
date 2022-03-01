c2d9a157edfc942f3dbd05e0d0cad38bd65b1448
sggongshow <87620031+sggongshow@users.noreply.github.com>
2022-02-28 21:38:06 -0800

    Update CumulativeLab3Mods.user.js

--- CumulativeLab3Mods.user.js
+++ CumulativeLab3Mods.user.js
@@ -7,7 +7,7 @@
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
 // @updateURL https://github.com/sggongshow/Oscars/raw/main/CumulativeLab3Mods.user.js
 // @downloadURL https://github.com/sggongshow/Oscars/raw/main/CumulativeLab3Mods.user.js
-// @version 21.02.28.5
+// @version 22.02.28.5
 // ==/UserScript==
 
 //https://doctors-office-surrey.kai-oscar.com/oscar///lab/CumulativeLabValues3.jsp?demographic_no=1507&toDate=&fromDate=&showAllReport=true
