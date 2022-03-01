e8367a08435b9e314128d646adeb7e6922c0b084
sggongshow <87620031+sggongshow@users.noreply.github.com>
2022-02-28 21:38:28 -0800

    Update DocumentReviewButtons.user.js

--- DocumentReviewButtons.user.js
+++ DocumentReviewButtons.user.js
@@ -8,7 +8,7 @@
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
 // @updateURL https://github.com/sggongshow/Oscar/raw/main/DocumentReviewButtons.user.js
 // @downloadURL https://github.com/sggongshow/Oscar/raw/main/DocumentReviewButtons.user.js
-// @version 21.02.28.5
+// @version 22.02.28.5
 // ==/UserScript==
 //window.moveTo(300, 100)
 
