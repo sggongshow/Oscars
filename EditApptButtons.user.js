6a03288825089208241ad6ffbd1869c2a16ae58d
sggongshow <87620031+sggongshow@users.noreply.github.com>
2022-02-28 21:39:46 -0800

    Update EditApptButtons.user.js

--- EditApptButtons.user.js
+++ EditApptButtons.user.js
@@ -4,7 +4,9 @@
 // @description Constant EForm Submit and Print button locations
 // @include     *appointmentcontrol.jsp*
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
-// @version 15.2
+// @updateURL https://github.com/sggongshow/Oscars/raw/main/EditApptButtons.user.js
+// @downloadURL https://github.com/sggongshow/Oscars/raw/main/EditApptButtons.user.js
+// @version 22.02.28.5
 // ==/UserScript==
 
 
