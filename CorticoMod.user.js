77b9d24481a846ea148f245e794f442c0cc0a49a
sggongshow <87620031+sggongshow@users.noreply.github.com>
2022-02-28 21:40:39 -0800

    Update CorticoMod.user.js

--- CorticoMod.user.js
+++ CorticoMod.user.js
@@ -6,7 +6,7 @@
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
 // @updateURL https://github.com/sggongshow/Oscars/raw/main/CorticoMod.user.js
 // @downloadURL https://github.com/sggongshow/Oscars/raw/main/CorticoMod.user.js
-// @version 21.02.28.5
+// @version 22.02.28.5
 // @grant       none
 // ==/UserScript==
 
