c5109b9b3497369bd762ba2cbc3837612e990b1d
sggongshow <87620031+sggongshow@users.noreply.github.com>
2022-02-28 19:46:31 -0800

    Update EChartButtons.user.js

--- EChartButtons.user.js
+++ EChartButtons.user.js
@@ -4,7 +4,8 @@
 // @description Various navigation buttons for echart screen.  Set your own specific fid (form number) or Measurement groupName
 // @include     */casemgmt/forward.jsp?action=view&demographic*
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
-// @version 15.2
+// @downloadURL https://github.com/sggongshow/Oscars/raw/main/EChartButtons.user.js
+// @version 21.02.28
 // @grant       none
 // ==/UserScript==
 //window.moveTo(300, 100)
